1535759da90cba1e4fe5fad668a2e00ca254c6aa
SuperAviation001 <superaviation001@gmail.com>
2025-05-27 19:06:44 +1000

    Revert "tetris-sepolicy: Rebrand to TETRIS and fix compilation on StatiXOS"
    This reverts commit 0232d90eaf68010cca94f6909fa390576af873dc.

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,17 +1,17 @@
 # Board specific SELinux policy variable definitions
-TETRIS_SEPOLICY_PATH := device/nothing/tetris-sepolicy
+MTK_SEPOLICY_PATH := device/nothing/tetris-sepolicy
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(TETRIS_SEPOLICY_PATH)/base/vendor \
-    $(TETRIS_SEPOLICY_PATH)/debug/vendor \
-    $(TETRIS_SEPOLICY_PATH)/third_party/vendor
+    $(MTK_SEPOLICY_PATH)/base/vendor \
+    $(MTK_SEPOLICY_PATH)/debug/vendor \
+    $(MTK_SEPOLICY_PATH)/third_party/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    $(TETRIS_SEPOLICY_PATH)/base/private \
-    $(TETRIS_SEPOLICY_PATH)/debug/private \
-    $(TETRIS_SEPOLICY_PATH)/third_party/private
+    $(MTK_SEPOLICY_PATH)/base/private \
+    $(MTK_SEPOLICY_PATH)/debug/private \
+    $(MTK_SEPOLICY_PATH)/third_party/private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(TETRIS_SEPOLICY_PATH)/base/public \
-    $(TETRIS_SEPOLICY_PATH)/debug/public \
-    $(TETRIS_SEPOLICY_PATH)/third_party/public
+    $(MTK_SEPOLICY_PATH)/base/public \
+    $(MTK_SEPOLICY_PATH)/debug/public \
+    $(MTK_SEPOLICY_PATH)/third_party/public
